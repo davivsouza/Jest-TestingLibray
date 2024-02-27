a368a60e249f81a8b3fe9058b9a0e520f76230cd
davivasconcelossouza21 <davivasconcelossouza21@gmail.com>
2024-02-27 12:59:14 +0000

    feat: testando waitFor e remove item from list

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,10 @@
-import { render } from "@testing-library/react"
+import { queryByText, render, waitFor, screen} from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
 import App from "./App"
 
 function setup(jsx:React.ReactNode ) {
     return {
       user: userEvent.setup(),
-      // Import `render` from the framework library of your choice.
-      // See https://testing-library.com/docs/dom-testing-library/install#wrappers
       ...render(jsx),
     }
   }
@@ -32,5 +30,17 @@ describe('<App/>', () => {
 
         expect(getByText('Python')).toBeInTheDocument()
        
+    })
+
+    it('should remove item from list', async () => {
+      const {user, getAllByText} = setup(<App/>)
+
+      const removeButtons = getAllByText('Remover')
+
+      user.click(removeButtons[0])
+
+      await waitFor(() => {
+        expect(screen.queryByText('Java')).not.toBeInTheDocument()
+      })
     })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,21 @@ function App() {
     setList(state => [...state, 'Python'])
   }
 
+  function removeFromList(item: string){
+    setTimeout(() => {
+      setList(state => state.filter(i => i !== item))
+    }, 500)
+  }
   return (
     <>
      <button onClick={addToList}>Add to list</button>
      <input placeholder="Novo item" type="text" />
      <ul>
       {list.map(item => (
-        <li key={item}>{item}</li>
+        <li key={item}>
+          {item}
+          <button onClick={() => removeFromList(item)}>Remover</button>
+        </li>
       ))}
      </ul>
     </>
